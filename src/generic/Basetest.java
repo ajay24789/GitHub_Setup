7c1f72a3864daf372971a19a5ef6375faaca41d6
Ajay <ajaykumar24789@gmail.com>
2017-05-28 13:00:23 +0530

    Commit 2 ..

--- src/generic/Basetest.java
+++ src/generic/Basetest.java
@@ -1,23 +1,32 @@
 package generic;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public abstract class Basetest implements AutoConstant {
 	
 	public WebDriver driver;
 	
-	// doing code change  
+	// Paramertisation done  
 	
+	@Parameters({"nodeURL","browser","AppURL"})
 	@BeforeMethod
-	public void openApp(){
-		System.setProperty(CHROME_KEY, CHROME_VALUE);
-		driver = new ChromeDriver();
-		driver.get("http://localhost/login.do");
+	public void openApp(String nodeURL, String browser, String AppURL) throws MalformedURLException
+	{
+		URL ra = new URL(nodeURL);
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName(browser);
+		driver = new RemoteWebDriver(ra, dc);
+		driver.get(AppURL);
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
